7e9435801c95509fd8a215a3a860ae178d4b792c
Jose Moran <jrmoran1993@gmail.com>
2023-03-17 17:05:04 +0100

    all tests pass

--- test_scraper/test_scraping_utilities.py
+++ test_scraper/test_scraping_utilities.py
@@ -1,6 +1,8 @@
 import pytest
+import requests
+from bs4 import BeautifulSoup
 
-from scraper.scraping_utilities import get_year, get_version_word, get_versions
+from scraper.scraping_utilities import get_year, get_version_word, get_versions, get_next_url
 
 
 @pytest.fixture(name="roi_url")
@@ -19,6 +21,16 @@ def test_get_word():
 
 
 def test_get_versions(roi_url):
-    version_words, version_years = get_versions(roi_url)
+    version_words, version_years, url = get_versions(roi_url)
     assert set(version_words) == {"roi", "roy"}
     assert set(version_years) == {1694, 1718, 1740, 1762, 1798, 1835, 1878, 1935, 2022}
+
+
+def test_next_url(roi_url):
+    query = requests.get(roi_url)
+    html = BeautifulSoup(query.text)
+    assert get_next_url(html, "roi") == "https://www.dictionnaire-academie.fr/article/A9R2840"
+
+    query2 = requests.get("https://www.dictionnaire-academie.fr/article/A6Z0043*")
+    html = BeautifulSoup(query2.text)
+    assert get_next_url(html, "zymotechnie") == "END"
