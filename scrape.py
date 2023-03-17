3a64443595a860a056c6c90fe42214b7721c1ff5
Jose Moran <jrmoran1993@gmail.com>
2023-03-17 17:40:15 +0100

    run script

--- scrape.py
+++ scrape.py
@@ -1,4 +1,3 @@
-from tqdm import tqdm
 import json
 
 from scraper.scraping_utilities import get_versions, get_article_id
@@ -9,15 +8,11 @@ current_url = "https://www.dictionnaire-academie.fr/article/A9A0001"
 next_url = ""
 
 
-def generator(condition):
-    while condition:
-        yield
-
-
-for _ in tqdm(generator(next_url != "END")):
+while next_url != "END":
     version_words, version_years, next_url = get_versions(current_url)
-    id = get_article_id(current_url)
-    scrape_dict[id] = {"words": version_words, "years": version_years}
+    print(version_words[-1], end=", ")
+    art_id = get_article_id(current_url)
+    scrape_dict[art_id] = {"words": version_words, "years": version_years}
     current_url = next_url
 
-json.dump(scrape_dict, open("./scrape_results.json", "w"))
+json.dump(scrape_dict, open("./scraped_dict.json", "w"))
